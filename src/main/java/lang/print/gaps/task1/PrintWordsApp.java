116d76a61b2b1581966f8f71679b09c64f7205b0
farxodovferuz33 <119694606+farxodovferuz33@users.noreply.github.com>
2023-01-04 13:13:12 -0500

    Update PrintWordsApp.java

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        System.out.println("WordsApp");
+        System.out.println("Java \nis \nice");
     }
 }
